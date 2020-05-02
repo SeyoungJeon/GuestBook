3c00ada81fe1c20d11087f505f87401e9896ef44
SeyoungJeon <34870901+SeyoungJeon@users.noreply.github.com>
2020-05-02 22:01:16 +0900

    [Add] Guestbook Service

---
+++ guestbook/src/main/java/kr/or/young/guestbook/service/impl/GuestbookServiceImpl.java
@@ -0,0 +1,68 @@
+package kr.or.young.guestbook.service.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import kr.or.young.guestbook.dao.GuestbookDao;
+import kr.or.young.guestbook.dao.LogDao;
+import kr.or.young.guestbook.dto.Guestbook;
+import kr.or.young.guestbook.dto.Log;
+import kr.or.young.guestbook.service.GuestbookService;
+
+@Service
+public class GuestbookServiceImpl implements GuestbookService{
+	
+	@Autowired
+	GuestbookDao guestbookDao;
+	
+	@Autowired
+	LogDao logDao;
+
+	@Override
+	@Transactional // Read Only
+	public List<Guestbook> getGuestbooks(Integer start) {
+		List<Guestbook> list = guestbookDao.selectAll(start, GuestbookService.LIMIT);
+		return list;
+	}
+
+	@Override
+	@Transactional(readOnly=false)
+	public int deleteGuestbook(Long id, String ip) {
+		int deleteCount = guestbookDao.deleteById(id);
+		Log log = new Log();
+		log.setId(id);
+		log.setIp(ip);
+		log.setMethod("delete");
+		log.setRegdate(new Date());
+		logDao.insert(log);
+		return deleteCount;
+	}
+
+	@Override
+	@Transactional(readOnly=false)
+	public Guestbook addGuestbook(Guestbook guestbook, String ip) {
+		guestbook.setRegdate(new Date());
+		Long id = guestbookDao.insert(guestbook);
+		guestbook.setId(id);
+		
+		Log log = new Log();
+		log.setId(id);
+		log.setIp(ip);
+		log.setMethod("insert");
+		log.setRegdate(new Date());
+		logDao.insert(log);
+		
+		return guestbook;
+	}
+
+	@Override
+	public int getCount() {
+		return guestbookDao.selectCount();
+	}
+
+	
+}
